2aa2f7d5b1c50025ef709913bcc88a304351754b
ArtemSadovenko <asadovenko7@gmail.com>
2024-11-01 03:31:32 +0200

    Some clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import AppRouter from './AppRouter';
+
 import Authenticator from './pages/Authenticator';
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Authenticator from './pages/Authenticator';
-import { Dashboard } from '@mui/icons-material';
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
